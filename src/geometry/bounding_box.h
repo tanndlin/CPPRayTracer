f9c7d780b7637460635bac13d02fc5053f074816
tanndlin <tanndlin@gmail.com>
2024-11-10 08:31:47 -0500

    Fix BVH for planes

--- src/geometry/bounding_box.h
+++ src/geometry/bounding_box.h
@@ -92,7 +92,10 @@ class bounding_box {
             tmax = std::min(tmax, std::max(std::max(t1, t2), tmin));
         }
 
-        return tmax > std::max(tmin, 0.0) && tmax > 0 ? tmin : -1;
+        if (tmin < 0)
+            return -1;
+
+        return tmax >= tmin ? tmin : -1;
     }
 
     void calc_points() {
